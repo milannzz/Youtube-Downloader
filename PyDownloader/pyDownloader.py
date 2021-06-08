bc501c6dfacefdcfd37636e3e29cc0aec0080a31
Milan Kandwal <milankandwal98@gmail.com>
2021-06-08 23:40:25 +0530

    Build_210608.2340
    Better UI.

--- PyDownloader/pyDownloader.py
+++ PyDownloader/pyDownloader.py
@@ -1,4 +1,3 @@
-from ssl import ALERT_DESCRIPTION_ACCESS_DENIED
 from tkinter import *
 from pytube  import YouTube
 from threading import Thread
@@ -8,31 +7,30 @@ import ctypes
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 root = Tk()
-
-
+root.geometry("500x300")
 root.title("Youtube Downloader")
 
 link = StringVar()
 
 label = Label(root,text="Paste Link Here ...",font ="arial 11 bold")
-label.place(x=200,y=20)
+label.grid(row=0,column=0,padx=20,pady=(30,10))
 
 entry = Entry(root,width=55,textvariable=link)
-label.place(x=20,y=40)
+entry.grid(row=1,column=0,padx=20,pady=10)
 
-def threadingg():
+def threading():
     thread= Thread(target = downloader)
     thread.start()
 
 def downloader():
     downloaded = Label(root,text = "Downloading...",font="arial 11",fg='#FFA500')
-    label.place(x=20,y=60)
+    downloaded.grid(row=2,column=0)
     url = YouTube(str(link.get()))
     video = url.streams.first()
     video.download()
     downloaded.config(text="Downloaded",fg='#008000')
 
-DwnButton = Button(root,text="Download",font="arial 11 bold",command=threadingg)
-label.place(x=200,y=80)
+DwnButton = Button(root,text="Download",font="arial 11 bold",command=threading)
+DwnButton.grid(row=3,column=0,padx=20,pady=10)
 
 root.mainloop()
