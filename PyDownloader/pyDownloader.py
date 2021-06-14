4082bf10a6c0b12098d592598262942cabf4c961
Milan Kandwal <milankandwal98@gmail.com>
2021-06-15 00:00:20 +0530

    Add files via upload

--- PyDownloader/pyDownloader.py
+++ PyDownloader/pyDownloader.py
@@ -10,11 +10,15 @@ ctypes.windll.shcore.SetProcessDpiAwareness(1)
 root = Tk()
 root.geometry("500x300")
 root.title("Youtube Downloader")
+root.grid_columnconfigure(0,weight = 1)
+root.grid_columnconfigure(1,weight = 1)
+root.grid_columnconfigure(2,weight = 1)
+root.grid_columnconfigure(3,weight = 1)
 
 link = StringVar()
 
 label = Label(root,text="Paste Link Here ...",font ="arial 11 bold")
-label.grid(row=0,column=0,padx=10,pady=(10,5))
+label.grid(row=0,column=0,padx=10,pady=5)
 
 entry = Entry(root,width=55,textvariable=link)
 entry.grid(row=1,column=0,padx=10,pady=5)
@@ -25,6 +29,7 @@ def threading():
 
 def downloader():
     global path
+    path = '~/Downloads'
     #https://www.youtube.com/watch?v=Wch3gJG2GJ4
     url = YouTube(str(link.get()))
     downloaded = Label(root,text = "Downloading - "+url.title,font="arial 11",fg='#FFA500')
@@ -37,7 +42,6 @@ def saveLoc() :
     global path
     path=filedialog.askdirectory()
     
-
 frame = Frame(root)
 frame.grid(row=4,column=0)
 
