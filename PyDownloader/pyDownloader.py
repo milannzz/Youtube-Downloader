e9f9cce9074556bd771dad356ae0ac6dcebbf9a2
Milan Kandwal <milankandwal98@gmail.com>
2021-06-14 23:43:12 +0530

    Build_210614.2342
    Now shows Video Titile.
    Added Save loacation button.
    Bug Fixes.

--- PyDownloader/pyDownloader.py
+++ PyDownloader/pyDownloader.py
@@ -13,24 +13,33 @@ root.title("Youtube Downloader")
 link = StringVar()
 
 label = Label(root,text="Paste Link Here ...",font ="arial 11 bold")
-label.grid(row=0,column=0,padx=20,pady=(30,10))
+label.grid(row=0,column=0,padx=10,pady=(10,5))
 
 entry = Entry(root,width=55,textvariable=link)
-entry.grid(row=1,column=0,padx=20,pady=10)
+entry.grid(row=1,column=0,padx=10,pady=5)
 
 def threading():
     thread= Thread(target = downloader)
     thread.start()
 
 def downloader():
-    downloaded = Label(root,text = "Downloading...",font="arial 11",fg='#FFA500')
-    downloaded.grid(row=2,column=0)
     url = YouTube(str(link.get()))
+    downloaded = Label(root,text = "Downloading - "+url.title,font="arial 11",fg='#FFA500')
+    downloaded.grid(row=2,column=0)
     video = url.streams.first()
     video.download()
-    downloaded.config(text="Downloaded",fg='#008000')
+    downloaded.config(text="Downloaded - "+url.title,fg='#008000')
 
-DwnButton = Button(root,text="Download",font="arial 11 bold",command=threading)
-DwnButton.grid(row=3,column=0,padx=20,pady=10)
+def saveLoc() :
+    pass
+
+frame = Frame(root)
+frame.grid(row=4,column=0)
+
+DwnButton = Button(frame,text="Download",font="arial 11 bold",command=threading)
+DwnButton.grid(row=0,column=0,padx=10,pady=5)
+
+saveButton = Button(frame,text="Save Location",font="arial 11 bold",command=saveLoc)
+saveButton.grid(row=0,column=1,padx=10,pady=5)
 
 root.mainloop()
