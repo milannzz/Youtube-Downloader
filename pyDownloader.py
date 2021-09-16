c77ff23b761e7d8e0a5a18ae7d2a9ab14829eb14
Milan Kandwal <milankandwal98@gmail.com>
2021-09-16 23:02:21 +0530

    Improved UI, Increased Performance, Bug Fixes.

--- pyDownloader.py
+++ pyDownloader.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from pytube  import YouTube
+from pytube  import YouTube, exceptions
 from threading import Thread
 from tkinter import filedialog, messagebox, ttk
 from pathlib import Path
@@ -15,13 +15,13 @@ def download_thread():
     thread= Thread(target = downloader)
     thread.start()
 
-def process_thread():
-    thread= Thread(target = Process)
+def get_qualities_thread():
+    thread= Thread(target = get_qualities)
     thread.start()
 
-def Process():
+def get_qualities():
     global path
-    status_string.set("Processing")
+    status_string.set("Gathering video information ...")
     try :
         status_string.set("Checking download path")
         path
@@ -32,129 +32,126 @@ def Process():
     #https://www.youtube.com/watch?v=Wch3gJG2GJ4 lowquality
     #https://www.youtube.com/watch?v=_jYB-RpkmUc highquality
 
-    root.grid_columnconfigure(2, weight = 1)
-
+    root_window.grid_columnconfigure(2, weight = 1)
+    downloading_status.config(fg='grey')
     try :
-        status_string.set("Getting info ...")
-        url = YouTube(str(link.get()))
+        status_string.set("Gathering video information ...")
+        url = YouTube(str(video_URL.get()))
     except :
-        messagebox.showerror("Error", "Invalid Video URL")
-        status_string.set("Failed,  Try again")
+        downloading_status.config(fg='red')
+        status_string.set("Invalid URL")
         return
 
+    quality_settings_list = []
     streams = url.streams
     for stream in streams:
         if(stream.type == "video"):
-            status_string.set("getting videos")
-            quality_settings.append(str(stream.itag) + " | " + "Video " + stream.resolution + " " + str(stream.fps) + "fps")
+            status_string.set("Processing video codecs")
+            quality_settings_list.append(str(stream.itag) + " | " + "Video " + stream.resolution + " " + str(stream.fps) + "fps")
         if(stream.type == "audio"):
-            status_string.set("getting audios")
-            quality_settings.append(str(stream.itag) + " | " + "Audio " + stream.abr)
-    quality_combo["values"] = quality_settings
-    status_string.set("Processed")
+            status_string.set("Processing audio codecs")
+            quality_settings_list.append(str(stream.itag) + " | " + "Audio " + stream.abr)
+    quality_combobox["values"] = quality_settings_list
+    status_string.set("Processing completed")
     downloading_status.config(fg='green')
-    root.update()
+    root_window.update()
 
 
 def downloader():
     global path
+    downloading_status.config(fg="grey")
+    status_string.set("Checking download path")
     try :
         path
     except NameError:
         downloads_path = str(Path.home() / "Downloads")
         path = downloads_path
-
-    #https://www.youtube.com/watch?v=Wch3gJG2GJ4
-
-    downloaded = Label(root, text = "Processing...", font="Helvetica 9 bold", fg='#808080')
-    downloaded.grid(row=2, column=0)
-    root.grid_columnconfigure(2, weight = 1)
+    status_string.set("Connecting to servers")
 
     try :
-        url = YouTube(str(link.get()))
+        url = YouTube(str(video_URL.get()))
     except :
-        messagebox.showerror("Error", "Invalid Video URL")
-        downloaded.config(text="")
+        downloading_status.config(fg='red')
+        status_string.set("Invalid URL")        
         return
 
-    if quality_combo.get() == "Auto" :
+    if quality_combobox.get() == "Auto" :
         video = url.streams.first()
     else :
-        itag = quality_combo.get()
+        itag = quality_combobox.get()
         s = ""
         for char in itag:
             if char == '|':
                 break
             s += char
         video = url.streams.get_by_itag(s)
-
-    downloaded.config(text = "Downloading: "+url.title+"| "+str(url.length / 60) +" min", fg='#FFA500')
-    
+    video_length = "{:.2f}".format(url.length / 60)
+    downloading_status.config(fg="orange")
+    status_string.set("Downloading :" + url.title + " | "+ video_length +" min ")
     try :
         video.download(path)
     except:
         messagebox.showerror("Error", "Network Error,  Please check your connection.")
         return
-
-    downloaded.config(text="Downloaded: "+url.title+"|"+str(url.length / 60) + " min", fg='#008000')
+    downloading_status.config(fg="green")
+    status_string.set("Downloaded :" + url.title + " | "+ video_length + " min ")
     
 def saveLoc() :
     global path
     path=filedialog.askdirectory()
 
-def quality():
-    pass
-
 def on_entry_click(*args):
-    if entry.get()=="":
-        paste = root.clipboard_get()
-        entry.insert(0, paste)
+    if URL_entry.get()=="":
+        paste = root_window.clipboard_get()
+        URL_entry.insert(0, paste)
+        get_qualities_thread()
 
 # <----------------------- UI --------------------------->
 
-root = Tk()
-root.geometry("600x200")
-root.title("Youtube Downloader")
-root.resizable(0, 0)
+root_window = Tk()
+root_window.geometry("630x180")
+root_window.title("Youtube Downloader")
+root_window.minsize(630, 180)
+root_window.resizable(1, 1)
 
-Label(root, text="Paste Link Here ...", font="Helvetica 9").grid(row=0, column=0, padx=10, pady=(10, 5))
-root.grid_columnconfigure(0, weight = 1)
+Label(root_window, text="Paste Link Here ...", font="Helvetica 9").grid(row=0, column=0, padx=10, pady=(10, 5))
+root_window.grid_columnconfigure(0, weight = 1)
+root_window.grid_rowconfigure(0, weight = 1)
 
-link = StringVar()
+video_URL = StringVar()
 
-entryFrame = Frame(root)
-entryFrame.grid(row=1, column=0, padx=10, pady=5)
-root.grid_columnconfigure(1, weight = 1)
+URL_container = Frame(root_window)
+URL_container.grid(row=1, column=0, padx=10, pady=5)
+root_window.grid_columnconfigure(1, weight = 1)
 
-entry = Entry(entryFrame, width=68, textvariable=link, font="Helvetica 9 italic")
-entry.grid(row=0, column=0, padx=10, ipady=5, ipadx=5, pady=5)
+URL_entry = Entry(URL_container, width=68, textvariable=video_URL, font="Helvetica 9 italic")
+URL_entry.grid(row=0, column=0, padx=10, ipady=5, ipadx=5, pady=5)
+URL_entry.bind('<Button-1>',  on_entry_click)
 
-entry.bind('<Button-1>',  on_entry_click)
-
-frame = Frame(root)
+frame = Frame(root_window)
 frame.grid(row=4, column=0, padx=10, pady=5)
-root.grid_columnconfigure(3, weight = 1)
+root_window.grid_columnconfigure(3, weight = 1)
 
 quality_label = Label(frame, text="Quality:", font="Helvetica 9")
-quality_label.grid(row=0, column=0)
+quality_label.grid(row=0, column=0, padx=0, pady=0, sticky=W)
 
 status_string = StringVar()
 status_string.set("")
-downloading_status = Label(root, textvariable=status_string, font="Helvetica 9 bold", fg='#808080')
+downloading_status = Label(root_window, textvariable=status_string, font="Helvetica 11", fg='#808080')
 downloading_status.grid(row=2, column=0)
 
-quality_settings = ["Auto"]
-quality_combo = ttk.Combobox(frame, values=quality_settings, font="Helvetica 9", width=16)
-quality_combo.grid(row=0, column=1, padx=10, pady=5, ipady=4)
-quality_combo.set("Auto")
+quality_settings_list = ["Auto"]
+quality_combobox = ttk.Combobox(frame, values=quality_settings_list, font="Helvetica 9", width=20)
+quality_combobox.grid(row=0, column=1, padx=10, pady=5, ipady=4)
+quality_combobox.set("Auto")
 
-process_button = Button(frame, text="Process", font="Helvetica 9", padx=8, pady=2, command=process_thread)
+process_button = Button(frame, text="Process", font="Helvetica 9", padx=8, pady=2, command=get_qualities_thread)
 process_button.grid(row=0, column=2, padx=10, pady=5)
 
-Download_button = Button(frame, text="Download", font="Helvetica 9", padx=8, pady=2, command=download_thread)
-Download_button.grid(row=0, column=3, padx=10, pady=5)
+download_button = Button(frame, text="Download", font="Helvetica 9", padx=8, pady=2, command=download_thread)
+download_button.grid(row=0, column=3, padx=10, pady=5)
 
 save_button = Button(frame, text="Save Location", font="Helvetica 9", padx=8, pady=2, command=saveLoc)
 save_button.grid(row=0, column=4, padx=10, pady=5)
 
-root.mainloop()
+root_window.mainloop()
